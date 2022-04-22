214773959c8a4f35fbe0de346181106756466111
SoyYuyin <emtzhd@gmail.com>
2022-04-22 16:57:02 -0600

    created Bullet class

--- settings.py
+++ settings.py
@@ -10,3 +10,9 @@ class Settings:
 
         # Ship settings
         self.ship_speed = 0.3
+
+        # Bullet settings
+        self.bullet_speed = 0.5
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
