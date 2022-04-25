3391c8e17720298e9e8a640b549bb99dda8add38
SoyYuyin <emtzhd@gmail.com>
2022-04-24 18:45:27 -0600

    Make Play button respond when clicked

--- alien_invasion.py
+++ alien_invasion.py
@@ -61,6 +61,14 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+
+    def _check_play_button(self, mouse_pos):
+        """Start a new game when the player clicks Play."""
+        if self.play_button.rect.collidepoint(mouse_pos):
+            self.stats.game_active = True
 
     def _check_keydown_events(self, event):
         """Respond to keypresses."""
